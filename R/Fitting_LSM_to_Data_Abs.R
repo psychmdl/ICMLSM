ae029cfd66860121feabd4e3d18d38c91ec177aa
Mark Lowry <mlowry@mail.usf.edu>
2018-12-14 21:30:50 -0500

    Normalizing Simulations

--- R/Fitting_LSM_to_Data_Abs.R
+++ R/Fitting_LSM_to_Data_Abs.R
@@ -28,7 +28,9 @@ LSM_Fit_Abs <- function(
     h = runif(1, .01, 10) #Language Activation parameter on switch trials = h*3
     x = LSMSimulation_Abs(Simulations = 1, Participants = 1, Num_SubBlocks = 8, Trial_Comparisons = c(1,2,3,4,5,6), Comp = 3.2, h1 = h, h2 = h, L1_Strength = L1Strength, L2_Strength = L1Strength, NoiseMu = .0001, NoiseTau = .0001, NoiseSigma = .001, p1 = competition)
     Simulated_Data1 <- x$Simulation.Results[seq(2, 12, 2),]$Mean.RT
+    Simulated_Data1 <- Simulated_Data1 - Simulated_Data1[1]
     Simulated_Data2 <- x$Simulation.Results[seq(1, 11, 2),]$Mean.RT
+    Simulated_Data2 <- Simulated_Data2 - Simulated_Data2[1]
     Simulated_Data3 <- data.frame(Simulated_Data1, Simulated_Data2, Simulation = i)
     Simulated_Data[[i]] <- Simulated_Data3
 
