07e73e303b8e2fe5198049021672fa4e450a1c00
Mark Lowry <mlowry@mail.usf.edu>
2020-05-29 11:40:46 -0400

    tweaking competition

--- R/Fitting_ICM_to_Data_Abs.R
+++ R/Fitting_ICM_to_Data_Abs.R
@@ -23,10 +23,10 @@ ICM_Fit_Abs <- function(
   RMS1 <- vector()
   RMS2 <- vector()
   for(i in 1:Num_Sims){
-    competition = runif(1, 3, 6)
+    competition = runif(1, 0.4, 0.95)
     L1Strength = runif(1, 1, 10)
     h = runif(1, 1, 10) #Language Activation parameter on switch trials = h*3
-    x = ICMSimulation_Abs(Simulations = 1, Participants = 1, Num_SubBlocks = 8, Trial_Comparisons = c(1,2,3,4,5,6), Comp = competition, h1 = h, h2 = h, L1_Strength = L1Strength, L2_Strength = L1Strength, NoiseMu = .0001, NoiseTau = .0001, NoiseSigma = .001)
+    x = ICMSimulation_Abs(Simulations = 1, Participants = 1, Num_SubBlocks = 8, Trial_Comparisons = c(1,2,3,4,5,6), Comp = 5.0, h1 = h, h2 = h, L1_Strength = L1Strength, L2_Strength = L1Strength, NoiseMu = .0001, NoiseTau = .0001, NoiseSigma = .001, p1 = competition)
     Simulated_Data1 <- x$Simulation.Results[seq(2, 12, 2),]$Mean.RT
     Simulated_Data1 <- Simulated_Data1 - Simulated_Data1[1]
     Simulated_Data2 <- x$Simulation.Results[seq(1, 11, 2),]$Mean.RT
