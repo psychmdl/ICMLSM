e867cd015396d583963f6922345d2ac64cd90004
Mark Lowry <mlowry@mail.usf.edu>
2020-06-01 23:53:07 -0400

    tinkering

--- R/Fitting_LSM_to_Data_Abs.R
+++ R/Fitting_LSM_to_Data_Abs.R
@@ -26,7 +26,7 @@ LSM_Fit_Abs <- function(
     competition = runif(1, 3, 10)
     L1Strength = runif(1, 15, 25)
     h = runif(1, 0.0001, 10) #Language Activation parameter on switch trials = h*3*L1_Strength
-    x = LSMSimulation_Abs(Simulations = 1, Participants = 1, Num_SubBlocks = 8, Trial_Comparisons = c(1,2,3,4,5,6), Comp = competition, h1 = h, h2 = h, L1_Strength = L1Strength, L2_Strength = L1Strength, NoiseMu = .0001, NoiseTau = .0001, NoiseSigma = .001, p1 = 0.5)
+    x = LSMSimulation_Abs(Simulations = 1, Participants = 1, Num_SubBlocks = 8, Trial_Comparisons = c(1,2,3,4,5,6), Comp = competition, h1 = h, h2 = h, L1_Strength = L1Strength, L2_Strength = L1Strength, NoiseMu = .0001, NoiseTau = .0001, NoiseSigma = .001, p1 = 0.9)
     Simulated_Data1 <- x$Simulation.Results[seq(2, 12, 2),]$Mean.RT
     Simulated_Data1 <- Simulated_Data1 - Simulated_Data1[1]
     Simulated_Data2 <- x$Simulation.Results[seq(1, 11, 2),]$Mean.RT
