437aea24ec140eb65fac6232e3ad77657fed6895
Mark Lowry <mlowry@mail.usf.edu>
2020-06-08 23:02:42 -0400

    bug fix

--- R/Fitting_LSM_to_Data_Abs.R
+++ R/Fitting_LSM_to_Data_Abs.R
@@ -25,7 +25,7 @@ LSM_Fit_Abs <- function(
   for(i in 1:Num_Sims){
     competition = runif(1, 0.5, 0.58)
     L1Strength = runif(1, 1, 10)
-    h = runif(1, 0.0001, 10) #Language Activation parameter on switch trials = h*3*L1_Strength
+    h = runif(1, 0.0001, 3) #Language Activation parameter on switch trials = h*3*L1_Strength
     x = LSMSimulation_Abs(Simulations = 1, Participants = 1, Num_SubBlocks = 8, Trial_Comparisons = c(1,2,3,4,5,6), Comp = competition, h1 = h, h2 = h, L1_Strength = L1Strength, L2_Strength = L1Strength, NoiseMu = .0001, NoiseTau = .0001, NoiseSigma = .001, p1 = 0.35, c= .01)
     Simulated_Data1 <- x$Simulation.Results[seq(2, 12, 2),]$Mean.RT
     Simulated_Data1 <- Simulated_Data1 - Simulated_Data1[1]
